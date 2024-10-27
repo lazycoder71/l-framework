f03252c8361c41dec624c381f790c3627c14cdef
lamnd <lamnd.bounce@gmail.com>
2024-10-27 17:48:00 +0700

    * Fix

---
+++ Common/LCollect/Step/LCollectStepAudio.cs
@@ -0,0 +1,28 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace LFramework
+{
+    public class LCollectStepAudio : LCollectStep
+    {
+        [HorizontalGroup]
+        [SerializeField] private bool _isInserted;
+
+        [HorizontalGroup]
+        [Min(0f), ShowIf("@_isInserted")]
+        [SerializeField]
+        private float _insertTime;
+
+        //[SerializeField] private AudioConfig _audio;
+
+        public override string DisplayName { get { return "Audio"; } }
+
+        public override void Apply(LCollectItem item)
+        {
+            //if (_isInserted)
+            //    item.sequence.InsertCallback(_insertTime, () => { AudioManager.Play(_audio); });
+            //else
+            //    item.sequence.AppendCallback(() => { AudioManager.Play(_audio); });
+        }
+    }
+}
